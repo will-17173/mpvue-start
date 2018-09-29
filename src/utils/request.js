682240459e9982f7bc48986f66dbfa2010501a54
Will.H <huangzhaojian@gmail.com>
2018-09-29 18:23:01 +0800

    utils

---
+++ src/utils/request.js
@@ -0,0 +1,50 @@
+const request = {
+  get(url, data) {
+    return new Promise((resolve, reject) => {
+      wx.request({
+        url: url,
+        data: data,
+        method: 'GET',
+        header: {
+          'Content-Type': 'application/json'
+        },
+        success: function (res) {
+          if (res.statusCode != 200) {
+            wx.showToast({
+              title: "网络出错，稍后再试",
+              icon: "none"
+            });
+            return false;
+          }
+          resolve(res.data);
+        },
+        fail: function (error) {
+          reject(error);
+        },
+        complete: function () {}
+      })
+    })
+  },
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      wx.request({
+        url: url,
+        data: data,
+        method: 'POST',
+        header: {
+          'Content-Type': 'application/json'
+        },
+        success: function (res) {
+          resolve(res.data);
+        },
+        fail: function (error) {
+          reject(error);
+        },
+        complete: function () {}
+      })
+    })
+  }
+}
+
+
+export default request;
---
+++ src/utils/system.js
@@ -0,0 +1,40 @@
+let res = wx.getSystemInfoSync();
+
+export const updataManager = () => {
+  const updataManager = wx.getUpdateManager();
+  updateManager.onCheckForUpdate(function (res) {
+    // 请求完新版本信息的回调
+    if (res.hasUpdate) {
+      console.log('有新版本啦！')
+    }
+  })
+
+  updateManager.onUpdateReady(function () {
+    wx.showModal({
+      title: '更新提示',
+      content: '有新版本啦！立即更新',
+      showCancel: false,
+      success: function (res) {
+        if (res.confirm) {
+          // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+          updateManager.applyUpdate()
+        }
+      }
+    })
+  })
+
+  updateManager.onUpdateFailed(function () {
+    // 新的版本下载失败
+  })
+}
+
+export default {
+  WIN_WIDTH: res.screenWidth,
+  WIN_HEIGHT: res.screenHeight,
+  IS_IOS: /ios/i.test(res.system),
+  IS_ANDROID = /android/i.test(res.system),
+  IS_IPX = /iphone x/i.test(res.model),
+  STATUS_BAR_HEIGHT = res.statusBarHeight,
+  DEFAULT_HEADER_HEIGHT = 46, // res.screenHeight - res.windowHeight - res.statusBarHeight
+  DEFAULT_CONTENT_HEIGHT = res.screenHeight - res.statusBarHeight - wx.SNS_DEFAULT_HEADER_HEIGHT
+}
