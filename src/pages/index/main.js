686b95d0d57be9fed656256e6572991c8c158e48
Will.H <huangzhaojian@gmail.com>
2018-10-09 15:01:34 +0800

    页面配置更新

--- src/pages/index/main.js
+++ src/pages/index/main.js
@@ -3,12 +3,3 @@ import App from './index';
 
 const app = new Vue(App);
 app.$mount();
-
-export default {
-  config: {
-    navigationBarTitleText: 'title',
-    usingComponents: {
-      'i-button': '~/iview/button/index',
-    },
-  },
-};
