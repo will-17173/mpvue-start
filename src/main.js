de871a0e473f937c567fd137f559d3c75ed184f4
lidan-miss <lidan.miss@gmail.com>
2018-10-12 19:04:00 +0800

    http库使用fly

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import App from './App';
 import store from './store';
+import api from './http/api';
 
 Vue.config.productionTip = false;
 App.mpType = 'app';
+Vue.use(api);
 Vue.prototype.$store = store;
 
 const app = new Vue(App);
