fe7246ea7e93dd0d678b54027f59323840c2c44d
Will.H <huangzhaojian@gmail.com>
2018-10-16 19:40:54 +0800

    添加minapp-api-promise

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import Vue from 'vue';
-import App from './App';
-import store from './store';
-import api from './http/api';
+import Vue from 'vue'
+import App from './App'
+import store from './store'
+import api from './http/api'
+import WXP from 'minapp-api-promise'
 
 // 初始化云开发
 wx.cloud.init({
@@ -14,6 +15,7 @@ App.mpType = 'app';
 Vue.use(api);
 Vue.prototype.$store = store;
 Vue.prototype.$bus = Bus;
+Vue.prototype.$wx = WXP;
 
 const app = new Vue(App);
 app.$mount();
